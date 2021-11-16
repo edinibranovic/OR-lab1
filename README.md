7b1e7b307a5234fed84a2907a7164a8ee458171e
edinibranovic <edin.ibranovic@gmail.com>
2021-11-16 09:35:56 +0100

    or 2.lab

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 1. OR laboratorijska vježba 
+2. OR laboratorijska vježba 
 
 Tema: Obuća
 
@@ -8,32 +8,31 @@ Autor: Edin Ibranović
 
 Verzija skupa podataka: 1.0
 
-Baza podataka: PostgreSQL 13.3
+Baza podataka: MySQL
 
 Jezik u kojemu se nalaze podaci: hrvatski, engleski
 
 Datum Predaje: 2.11.2021
 
-Datum zadnjih izmjena: 2.11.2021
+Datum zadnjih izmjena: 16.11.2021
 
 Entiteti i pripadajući atributi:
-
     OBUCA
         id - jedinstvena oznaka, INT, primarni ključ
-        marka - proizvođač obuće, VARCHAR(20)
-        model - naziv kolekcije kojoj obuća pripada, VARCHAR(20)
-        spol - spol osoba kojima je obuća namijenjena, VARCHAR(3)
+        marka - proizvođač obuće, text
+        model - naziv kolekcije kojoj obuća pripada, text
+        spol - spol osoba kojima je obuća namijenjena, text
         velicina - EU velicina obuće, INT
         godProizvodnje - godina izlaska na tržište, INT
-        materijal - glavni gradivni materijal, VARCHAR(15)
-        vrsta - aktivnost kojoj je obuća namijenjena, VARCHAR(30)
-        visinaDona - opis visine đona, VARCHAR(10)
-        tipZatvaranja - način na koji se obuća zatvara, VARCHAR(10)
-        
+        materijal - glavni gradivni materijal, text
+        vrsta - aktivnost kojoj je obuća namijenjena, text
+        visinaDona - opis visine đona, text
+        tipZatvaranja - način na koji se obuća zatvara, text
+
     BOJA
         id - jedinstvena oznaka, INT, primarni ključ
-        boja - naziv boje, VARCHAR(15)
-        
+        boja - naziv boje, text
+
     IMA
     - postoji jer OBUCA i BOJA imaju many-to-many povezanost
     - par obuće može imati jednu ili više različitih boja, dok jedna boja može biti u jednom ili više parova obuće
--- obuca.sql
+++ obuca.sql
@@ -1,171 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- PostgreSQL database dump
---
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2021 at 09:29 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
--- Dumped from database version 13.3
--- Dumped by pg_dump version 13.3
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
 
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Name: boja; Type: TABLE; Schema: public; Owner: postgres
+-- Database: `obuca`
 --
 
-CREATE TABLE public.boja (
-    id integer NOT NULL,
-    boja character varying(15)
-);
-
-
-ALTER TABLE public.boja OWNER TO postgres;
+-- --------------------------------------------------------
 
 --
--- Name: ima; Type: TABLE; Schema: public; Owner: postgres
+-- Table structure for table `boja`
 --
 
-CREATE TABLE public.ima (
-    bojaid integer NOT NULL,
-    obucaid integer NOT NULL
-);
-
-
-ALTER TABLE public.ima OWNER TO postgres;
-
-CREATE TABLE public.obuca (
-    id integer NOT NULL,
-    marka character varying(20) NOT NULL,
-    model character varying(20) NOT NULL,
-    spol character varying(3) NOT NULL,
-    velicina integer NOT NULL,
-    godProizvodnje integer NOT NULL,
-    materijal character varying(15) NOT NULL,
-    vrsta character varying(30) NOT NULL,
-    visinaDona character varying(10) NOT NULL,
-    tipZatvaranja character varying(10) NOT NULL
-);
-
-ALTER TABLE public.obuca OWNER TO postgres;
+CREATE TABLE `boja` (
+  `id` int(11) NOT NULL,
+  `boja` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Data for Name: boja; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Dumping data for table `boja`
 --
 
-COPY public.boja (id, boja) FROM stdin; 
-1   Bijela
-2   Crna
-3   Plava
-4   Crvena
-5   Zuta
-6   Zelena
-7   Narancasta
-8   Ljubicasta
-9   Ruzicasta
-10  Zlatna
-11  Siva
-\.
+INSERT INTO `boja` (`id`, `boja`) VALUES
+(1, 'Bijela'),
+(2, 'Crna'),
+(3, 'Plava'),
+(4, 'Crvena'),
+(5, 'zuta'),
+(6, 'Zelena'),
+(7, 'Narancasta'),
+(8, 'Ljubicasta'),
+(9, 'Ruzicasta'),
+(10, 'Zlatna'),
+(11, 'Siva');
 
-
---
--- Data for Name: ima; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.ima (bojaid, obucaid) FROM stdin; 
-1   1
-1   7
-1   8
-1   9
-1   10
-2   2
-2   9
-2   10
-3   3
-3   10
-4   2
-5   5
-6   4
-6   5
-7   5
-7   4
-7   7
-8   6
-8   7
-9   7
-10  1
-11  8
-\.
-
+-- --------------------------------------------------------
 
 --
--- Data for Name: obuca; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Table structure for table `ima`
 --
 
-COPY public.obuca (id, marka, model, spol, velicina, godProizvodnje, materijal, vrsta, visinaDona, tipZatvaranja) FROM stdin; 
-1   Nike    Air Force    M    45    2017    Koza    Casual Tenisice    Visok    Vezice
-2   Nike    Air Max      Z    40    2016    Tekstil Sportske Tenisice  Visok    Vezice
-3   Nike    Air Jordan   M    48    2003    Koza    Sportske Tenisice  Visok    Vezice
-4   Adidas  Gazelle      UNI  42    2012    Sintetika    Casual Tenisice    Srednji    Vezice
-5   Adidas  Spezial      M    44    2010    Sintetika    Casual Tenisice    Nizak      Vezice
-6   Puma    Rider        Z    41    2020    Najlon       Casual Tenisice    Srednji    Vezice
-7   Puma    Smash        UNI  43    2020    Najlon       Casual Tenisice    Srednji    Vezice
-8   New Balance    NYCM  Z    42    2015    Sintetika    Tenisice za trcanje    Srednji     Vezice
-9   Karl Lagerfeld Volt Aktiv    M    47    2019    Najlon    Sandale    Nizak    Otvoreno
-10  Umbro          Medusae 3     M    46    2021    Koza      Kopacke    Visok    Bez vezica
-\.
-
+CREATE TABLE `ima` (
+  `bojaid` int(11) NOT NULL,
+  `obucaid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Name: boja boja_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Dumping data for table `ima`
 --
 
-ALTER TABLE ONLY public.boja 
-    ADD CONSTRAINT boja_pk PRIMARY KEY (id);
+INSERT INTO `ima` (`bojaid`, `obucaid`) VALUES
+(1, 1),
+(1, 7),
+(1, 8),
+(1, 9),
+(1, 10),
+(2, 2),
+(2, 9),
+(2, 10),
+(3, 3),
+(3, 10),
+(4, 2),
+(5, 5),
+(6, 4),
+(6, 5),
+(7, 4),
+(7, 5),
+(7, 7),
+(8, 6),
+(8, 7),
+(9, 7),
+(10, 1),
+(11, 8);
 
-
---
--- Name: ima ima_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.ima 
-    ADD CONSTRAINT ima_pk PRIMARY KEY (bojaid, obucaid);
-
-
---
--- Name: obuca obuca_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.obuca 
-    ADD CONSTRAINT obuca_pk PRIMARY KEY (id);
-
-
---
--- Name: ima_bojaid_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.ima 
-    ADD CONSTRAINT ima_bojaid_fk FOREIGN KEY (bojaid) REFERENCES public.boja(id);
-
+-- --------------------------------------------------------
 
 --
--- Name: ima_obucaid_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+-- Table structure for table `obuca`
 --
 
-ALTER TABLE ONLY public.ima 
-    ADD CONSTRAINT ima_obucaid_fk FOREIGN KEY (obucaid) REFERENCES public.obuca(id);
-
+CREATE TABLE `obuca` (
+  `id` int(11) NOT NULL,
+  `marka` text NOT NULL,
+  `model` text NOT NULL,
+  `spol` text NOT NULL,
+  `velicina` int(11) NOT NULL,
+  `godProizvodnje` int(11) NOT NULL,
+  `materijal` text NOT NULL,
+  `vrsta` text NOT NULL,
+  `visinaDona` text NOT NULL,
+  `tipZatvaranja` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- PostgreSQL database dump complete
+-- Dumping data for table `obuca`
 --
+
+INSERT INTO `obuca` (`id`, `marka`, `model`, `spol`, `velicina`, `godProizvodnje`, `materijal`, `vrsta`, `visinaDona`, `tipZatvaranja`) VALUES
+(1, 'Nike', 'Air Force', 'M', 45, 2017, 'Koza', 'Casual Tenisice', 'Visok', 'Vezice'),
+(2, 'Nike', 'Air Max', 'z', 40, 2016, 'Tekstil', 'Sportske Tenisice', 'Visok', 'Vezice'),
+(3, 'Nike', 'Air Jordan', 'M', 48, 2003, 'Koza', 'Sportske Tenisice', 'Visok', 'Vezice'),
+(4, 'Adidas', 'Gazelle', 'UNI', 42, 2012, 'Sintetika', 'Casual Tenisice', 'Srednji', 'Vezice'),
+(5, 'Adidas', 'Spezial', 'M', 44, 2010, 'Sintetika', 'Casual Tenisice', 'Nizak', 'Vezice'),
+(6, 'Puma', 'Rider', 'z', 41, 2020, 'Najlon', 'Casual Tenisice', 'Srednji', 'Vezice'),
+(7, 'Puma', 'Smash', 'UNI', 43, 2020, 'Najlon', 'Casual Tenisice', 'Srednji', 'Vezice'),
+(8, 'New Balance', 'NYCM', 'z', 42, 2019, 'Sintetika', 'Tenisice za trcanje', 'Srednji', 'Vezice'),
+(9, 'Karl Lagerfeld', 'Volt Aktiv', 'M', 47, 2021, 'Najlon', 'Sandale', 'Nizak', 'Otvoreno'),
+(10, 'Umbro', 'Medusae 3', 'M', 46, 2020, 'Koza', 'Kopacke', 'Visok', 'Slip-on');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `boja`
+--
+ALTER TABLE `boja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ima`
+--
+ALTER TABLE `ima`
+  ADD PRIMARY KEY (`bojaid`,`obucaid`),
+  ADD KEY `ima_obucaid_fk` (`obucaid`);
+
+--
+-- Indexes for table `obuca`
+--
+ALTER TABLE `obuca`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ima`
+--
+ALTER TABLE `ima`
+  ADD CONSTRAINT `ima_bojaid_fk` FOREIGN KEY (`bojaid`) REFERENCES `boja` (`id`),
+  ADD CONSTRAINT `ima_obucaid_fk` FOREIGN KEY (`obucaid`) REFERENCES `obuca` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
